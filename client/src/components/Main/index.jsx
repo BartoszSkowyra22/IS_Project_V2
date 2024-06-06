1e98ea0cf3655a68623e2d6ada213a94a68cedc1
BartoszSkowyra22 <skowyrab@gmail.com>
2024-06-06 21:16:57 +0200

    First changes in frontend

--- client/src/components/Main/index.jsx
+++ client/src/components/Main/index.jsx
@@ -1,16 +1,16 @@
 import styles from "./styles.module.css";
-import {useState} from "react";
-import {useNavigate, Link} from "react-router-dom";
-import RecipeList from "../RecipeList";
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import FlightList from "../FlightList";
 
-const categories = ['Śniadanie', 'Zupa', 'Obiad', 'Kolacja', 'Deser'];
+const filters = ['DestCityName', 'OriginCityName', 'FlightNum', 'FlightDelayType'];
 
 const Main = () => {
     const navigate = useNavigate();
-    const [selectedCategory, setSelectedCategory] = useState("");
+    const [selectedFilter, setSelectedFilter] = useState("");
 
     const handleChange = (e) => {
-        setSelectedCategory(e.target.value);
+        setSelectedFilter(e.target.value);
     };
 
     const handleLogout = () => {
@@ -21,27 +21,78 @@ const Main = () => {
     return (
         <div className={styles.main_container}>
             <nav className={styles.navbar}>
-                <h1>Moje przepisy</h1>
-                <Link to="/addrecipe" className={styles.white_btn}>Nowy przepis</Link>
+                <h1>Moje Loty</h1>
+                <Link to="/addflight" className={styles.white_btn}>Nowy lot</Link>
                 <button className={styles.white_btn} onClick={handleLogout}>
                     Wyloguj
                 </button>
             </nav>
             <select
-                name="category"
-                value={selectedCategory}
+                name="filter"
+                value={selectedFilter}
                 onChange={handleChange}
                 required
                 className={styles.input}
             >
-                <option value="">Wybierz kategorię</option>
-                {categories.map(category => (
-                    <option key={category} value={category}>{category}</option>
+                <option value="">Wybierz filtr</option>
+                {filters.map(filter => (
+                    <option key={filter} value={filter}>{filter}</option>
                 ))}
             </select>
-            {/*<RecipeList selectedCategory={selectedCategory}/>*/}
+            {<FlightList selectedFilter={selectedFilter} />}
         </div>
     );
 };
 
 export default Main;
+
+
+
+
+// import styles from "./styles.module.css";
+// import {useState} from "react";
+// import {useNavigate, Link} from "react-router-dom";
+// import RecipeList from "../RecipeList";
+//
+// const categories = ['Śniadanie', 'Zupa', 'Obiad', 'Kolacja', 'Deser'];
+//
+// const Main = () => {
+//     const navigate = useNavigate();
+//     const [selectedCategory, setSelectedCategory] = useState("");
+//
+//     const handleChange = (e) => {
+//         setSelectedCategory(e.target.value);
+//     };
+//
+//     const handleLogout = () => {
+//         localStorage.removeItem("token");
+//         navigate("/login");
+//     };
+//
+//     return (
+//         <div className={styles.main_container}>
+//             <nav className={styles.navbar}>
+//                 <h1>Moje przepisy</h1>
+//                 <Link to="/addrecipe" className={styles.white_btn}>Nowy przepis</Link>
+//                 <button className={styles.white_btn} onClick={handleLogout}>
+//                     Wyloguj
+//                 </button>
+//             </nav>
+//             <select
+//                 name="category"
+//                 value={selectedCategory}
+//                 onChange={handleChange}
+//                 required
+//                 className={styles.input}
+//             >
+//                 <option value="">Wybierz kategorię</option>
+//                 {categories.map(category => (
+//                     <option key={category} value={category}>{category}</option>
+//                 ))}
+//             </select>
+//             {/*<RecipeList selectedCategory={selectedCategory}/>*/}
+//         </div>
+//     );
+// };
+//
+// export default Main;
